7e090a26d53aef081517c00643bdd379b17e4d97
Luna-slendy <154507618+Luna-slendy@users.noreply.github.com>
2025-03-05 10:10:32 +0000

    tst

---
+++ stacks.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Stack{
+    private:
+    vector<int> data;
+
+    public:
+    boot isEmpty(){return data.empty();}
+    int size(){return data.size();}
+    void push(int value){data.push_back(value);}
+    int peek(){return.data_back(value);}
+    int pop(){
+        int value_to_return = peek();
+        data.pop_back();
+        return value_to_return;
+    }
+}
+
+int main(){
+    Stack stack;
+    stack.push(10);
+    cout << "peek intro stack =" << stack.peek() << endl;
+    for (int i = 0; i= 100; i++){
+        stack.push(i);
+    }
+    cout << "size of stack =" << stack.size() << endl;
+    cour << "peeking intro stack =" << stack.peek() << endl;
+    
+    for(;stack.size();){
+        cout << "value from stack" << stack pop() << endl;
+    }
+}
